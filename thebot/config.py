f83e272aa21b5cd5f1d4a57aee6560fd902a2dc9
akikoazumi <86140455+akikoazumi@users.noreply.github.com>
2021-07-19 20:03:38 +0700

    Update config.py

--- thebot/config.py
+++ thebot/config.py
@@ -1,8 +1,8 @@
 class Config(object):
     LOGGER = True
-    API_ID = 69
-    API_HASH = "owo"
-    TOKEN = "uwu"
+    API_ID = 3261733
+    API_HASH = "240b164f1b8082e49e87793ac6594da9"
+    TOKEN = "1605194389:AAFhgteAnU--yRbWaEt13GJXQ0kIPZRkzMg"
     DB_URI = ""
     LOG_CHANNEL = 0 # if you want a logging channel you can add this, else logs will go into Owner's PM
 
