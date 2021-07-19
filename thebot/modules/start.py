c271fbf429fb0ff3c8c69c2b07e58a658472f1a5
akikoazumi <86140455+akikoazumi@users.noreply.github.com>
2021-07-19 19:28:13 +0700

    Update start.py

--- thebot/modules/start.py
+++ thebot/modules/start.py
@@ -16,21 +16,21 @@ async def start(_, message):
                      [
                         InlineKeyboardButton('Anime', switch_inline_query_current_chat='anime '),
                         InlineKeyboardButton('Manga', switch_inline_query_current_chat='manga '),
-                        InlineKeyboardButton('nHentai', switch_inline_query_current_chat='nhentai ')
+ 
                      ],
                      [
                         InlineKeyboardButton('Airing', switch_inline_query_current_chat='airing '),
                         InlineKeyboardButton('Character', switch_inline_query_current_chat='char ')
                      ]
                   ]
-          await message.reply_text('Available cmds for now :\n /animeinfo - search anime on AniList\n /mangainfo - search manga on Anilist\n /charinfo - search character on Anilist\n /airinfo - check airing status of an anime\n /wa by replying to a media - find what anime a media is from\n /nhentai ID - returns the nhentai in telegraph instant preview format.', reply_markup=InlineKeyboardMarkup(buttons))
+          await message.reply_text('Available cmds for now :\n /animeinfo - search anime on AniList\n /mangainfo - search manga on Anilist\n /charinfo - search character on Anilist\n /airinfo - check airing status of an anime\n 👉 join : @akarida.', reply_markup=InlineKeyboardMarkup(buttons))
      else:
-       photo = "https://telegra.ph/file/19dad86d7b1009f1d6911.jpg"
+       photo = "https://telegra.ph/file/d2b024e3328e5b2ad52f5.jpg"
        buttons = [
             [
             InlineKeyboardButton('Anime', switch_inline_query_current_chat='anime '),
             InlineKeyboardButton('Manga', switch_inline_query_current_chat='manga '),
-            InlineKeyboardButton('nHentai', switch_inline_query_current_chat='nhentai ')
+
             ],
             [
             InlineKeyboardButton('Airing', switch_inline_query_current_chat='airing '),
@@ -44,4 +44,4 @@ async def start(_, message):
          caption='Hi, uwu >//////<\nCheck Help to know what I can do ;)\nSeach in Inline by Clicking these buttons below!',
          reply_markup=InlineKeyboardMarkup(buttons))
    else:
-       await message.reply_text("Hi there, I'm EsseX.")
+       await message.reply_text("Hi there, I'm kachi.")
