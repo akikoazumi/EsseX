10c1ad81e0bb4a320ec8e25a83a94b70173ec627
akikoazumi <86140455+akikoazumi@users.noreply.github.com>
2021-07-22 19:19:40 +0700

    Update config.py

--- thebot/config.py
+++ thebot/config.py
@@ -1,9 +1,9 @@
 class Config(object):
     LOGGER = True
     API_ID = 3261733
-    API_HASH = "240b164f1b8082e49e87793ac6594da9"
-    TOKEN = "1605194389:AAFhgteAnU--yRbWaEt13GJXQ0kIPZRkzMg"
-    DB_URI = "postgres://ulfcpxkc:zqBMxEiu7VfwdMjnB2qpZEKySNsnBuhc@arjuna.db.elephantsql.com/ulfcpxkc"
+    API_HASH = 240b164f1b8082e49e87793ac6594da9
+    TOKEN = 1605194389:AAEbTINNXiLV5Wq6UpkFqMAeqYl6RGsQFe8
+    DB_URI = ""
     LOG_CHANNEL = 0 # if you want a logging channel you can add this, else logs will go into Owner's PM
 
 class Development(Config):
